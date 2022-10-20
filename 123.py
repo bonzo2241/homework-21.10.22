eb90c178bb8cb1e54f89f4e7b346ce0da8b11069
bonzo2241 <bonzo2241@gmail.com>
2022-10-20 20:38:33 +0300

    123

--- 123.py
+++ 123.py
@@ -1,10 +1,16 @@
-a = int(input("Введите число"))
-b = int(input("В двоичную или восьмиричную?(введите цифру)"))
-c = ''
-if b == 2 or b == 8:
-  while a > 0:
-      c = str(a % b) + c
-      a //= b
-  print(c)
-else:
-    print("Попробуйте заново")
+def max(a, b):
+    if a>b:
+       b = a
+    return b
+
+
+
+
+r=int(input("Введите количество чисел"))
+A = [0] * r
+k=-100000000000000000000000000000000000000000000000000000000000000
+for i in range (r):
+    print(i+1, "Введите число:",)
+    A[i]=float(input())
+    k=max(A[i], k)    
+print(k)
